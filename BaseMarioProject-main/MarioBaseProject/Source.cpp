c87a2c3eec61526eb6af4bc02e676492ea1b96b4
OG-Pugsly <68538766+OG-Pugsly@users.noreply.github.com>
2023-02-17 09:44:04 +0000

    Display window creation

--- BaseMarioProject-main/MarioBaseProject/Source.cpp
+++ BaseMarioProject-main/MarioBaseProject/Source.cpp
@@ -3,8 +3,64 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include "constants.h"
+#include <iostream>
+
+using namespace std;
+
+//Globals
+SDL_Window* g_window = nullptr;
+
+//Function prototypes
+bool InitSDL();
+void CloseSDL();
 
 int main(int argc, char* args[])
 {
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+
+	CloseSDL();
 	return 0;
+}
+
+
+bool InitSDL()
+{
+	//Setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		//Setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//Did the window get created?
+		if (g_window == nullptr)
+		{
+			//Window failed
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+	}
+}
+
+void CloseSDL()
+{
+	//Release the window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
---
+++ BaseMarioProject-main/MarioBaseProject/constants.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//Screen Dimensions
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 416
