731c57860544ca904523066d542e5ce39106c225
OG-Pugsly <68538766+OG-Pugsly@users.noreply.github.com>
2023-02-24 10:14:37 +0000

    Image Functions
    Image loading, rendering and the memory management associated to this task.

--- BaseMarioProject-main/MarioBaseProject/Source.cpp
+++ BaseMarioProject-main/MarioBaseProject/Source.cpp
@@ -31,6 +31,7 @@ int main(int argc, char* args[])
 		//Game Loop
 		while (!quit)
 		{
+			Render();
 			quit = Update();
 		}
 	}
@@ -82,6 +83,12 @@ bool InitSDL()
 			return false;
 		}
 
+		//Load the background texture
+		g_texture = LoadTextureFromFile("Images/test.bmp");
+		if (g_texture == nullptr)
+		{
+			return false;
+		}
 	}
 }
 
@@ -90,6 +97,12 @@ void CloseSDL()
 	//Release the window
 	SDL_DestroyWindow(g_window);
 	g_window = nullptr;
+
+	//Clear the texture
+	FreeTexture();
+	//Release the renderer
+	SDL_DestroyRenderer(g_renderer);
+	g_renderer = nullptr;
 
 	//Quit SDL subsystems
 	IMG_Quit();
@@ -114,4 +127,59 @@ bool Update()
 	}
 
 	return false;
+}
+
+void Render()
+{
+	//Clear the screen
+	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(g_renderer);
+
+	//Set where to render the texture
+	SDL_Rect renderLocation = { 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
+
+	//Render to Screen
+	SDL_RenderCopyEx(g_renderer, g_texture, NULL, &renderLocation, 0, NULL, SDL_FLIP_NONE);
+
+	//Update the screen
+	SDL_RenderPresent(g_renderer);
+}
+
+SDL_Texture* LoadTextureFromFile(string path)
+{
+	//Remove memory used for a previous texture
+	FreeTexture();
+
+	SDL_Texture* p_texture = nullptr;
+
+	//Load the image
+	SDL_Surface* p_surface = IMG_Load(path.c_str());
+	if (p_surface != nullptr)
+	{
+		//Create the texture from the pixels on the surface
+		p_texture = SDL_CreateTextureFromSurface(g_renderer, p_surface);
+		if (p_surface == nullptr)
+		{
+			cout << "Unable to create texture from surface. Error: " << SDL_GetError();
+		}
+		//Remove the loaded surface now that we have a texture
+		SDL_FreeSurface(p_surface);
+	}
+	else
+	{
+		cout << "Unable to create texture from surface. Error: " << IMG_GetError();
+	}
+
+	//Return the texture
+	return p_texture;
+}
+
+void FreeTexture()
+{
+	//Check if texture exists before removing it
+	if (g_texture != nullptr)
+	{
+		SDL_DestroyTexture(g_texture);
+		g_texture = nullptr;
+	}
 }
