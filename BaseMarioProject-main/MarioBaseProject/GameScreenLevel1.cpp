f9e3093c21fc40e1f6593aa5defa204b43f883e1
OG-Pugsly <68538766+OG-Pugsly@users.noreply.github.com>
2023-03-10 10:58:12 +0000

    Initial movement
    Movement currently not working

--- BaseMarioProject-main/MarioBaseProject/GameScreenLevel1.cpp
+++ BaseMarioProject-main/MarioBaseProject/GameScreenLevel1.cpp
@@ -1,6 +1,8 @@
 #include "GameScreenLevel1.h"
 #include <iostream>
 #include "Texture2D.h"
+#include "Character.h"
+
 
 
 GameScreenLevel1::GameScreenLevel1(SDL_Renderer* renderer) : GameScreen(renderer)
@@ -12,17 +14,24 @@ GameScreenLevel1::~GameScreenLevel1()
 {
 	delete m_background_texture;
 	m_background_texture = nullptr;
+
+	delete my_character;
+	my_character = nullptr;
 }
 
 void GameScreenLevel1::Render()
 {
 	//Draw the background
 	m_background_texture->Render(Vector2D(), SDL_FLIP_NONE);
+
+	//Update Character
+	my_character->Render();
 }
 
-void GameScreenLevel1::Update(float delta, SDL_Event e)
+void GameScreenLevel1::Update(float deltaTime, SDL_Event e)
 {
-
+	//Update Character
+	my_character->Update(deltaTime, e);
 }
 
 bool GameScreenLevel1::SetUpLevel()
@@ -34,5 +43,7 @@ bool GameScreenLevel1::SetUpLevel()
 		std::cout << "Failed to load background texture!" << std::endl;
 		return false;
 	}
-	return true;
+
+	//Set up Player Character
+	my_character = new Character(m_renderer, "Images/Sprites/Mario.png", Vector2D(64, 330));
 }
--- BaseMarioProject-main/MarioBaseProject/GameScreenLevel1.h
+++ BaseMarioProject-main/MarioBaseProject/GameScreenLevel1.h
@@ -4,8 +4,11 @@
 
 #include "GameScreen.h"
 #include "Commons.h"
+#include "Character.h"
 
 class Texture2D;
+
+class Character;
 
 class GameScreenLevel1 : GameScreen
 {
@@ -18,6 +21,7 @@ public:
 
 private:
 	Texture2D* m_background_texture;
+	Character* my_character;
 
 	bool SetUpLevel();
 };
